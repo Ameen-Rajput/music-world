2f207de9d6ae26e35701a7942a62634f94ee4dd2
Ameen Rajput <rajputameen53@gmail.com>
2025-12-18 23:56:39 +0500

    Initial commit - Music World website

---
+++ index.js
@@ -0,0 +1,10 @@
+AOS.init();
+
+function showLoginAlert() {
+  alert("Please sign in to download this album.");
+}
+ function toggleMenu() {
+  const nav = document.getElementById("navMenu");
+  nav.classList.toggle("active");
+}
+
